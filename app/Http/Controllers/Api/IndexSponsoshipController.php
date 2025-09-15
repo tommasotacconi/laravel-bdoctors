b3f296ac363194fb9870eabf41ac9cb935145ae9
tommasoTacconi <tommaso.tacconi@gmail.com>
2025-09-15 17:08:39 +0200

    refactor: Remove commented and now used logic inside sponsored in IndexSponsorshipController

--- app/Http/Controllers/Api/IndexSponsoshipController.php
+++ app/Http/Controllers/Api/IndexSponsoshipController.php
@@ -53,53 +53,6 @@ class IndexSponsoshipController extends Controller
             'active_sponsorship.pivot.profile_id',
             'active_sponsorship.pivot.sponsorship_id',
         ]);
-        // $profile->user->makeHidden(['id', 'email', 'email_verified_at', 'home_address', 'created_at', 'updated_at']);
-        // -for specializations
-        // if ($profile->user->relationLoaded('specializations') && $profile->user->specializations) {
-        //     $profile->user->specializations->makeHidden(['id', 'created_at', 'updated_at', 'pivot']);
-        // }
-        // // Build profiles array, divided per sponsorship's type
-        // $profilesInSponsorships = array();
-        // foreach ($sponsorshipsWithProfiles as $sponsorshipWithProfiles) {
-        //     $sponsorship_name = $sponsorshipWithProfiles->name;
-        //     foreach ($sponsorshipWithProfiles->profiles as $profile) {
-        //         // Add sponsorship name as a new attribute
-        //         $profile->allSponsorships = [];
-        //         $profile->allSponsorships = array_merge($profile->allSponsorships, [$sponsorship_name]);
-        //         // Consider if the profile is already present by another sponsorship
-        //         $exist = false;
-        //         $sameProfile = null;
-        //         foreach ($profilesInSponsorships as $existingProfile) {
-        //             if ($existingProfile->id === $profile->id) {
-        //                 $exist = true;
-        //                 $sameProfile = $existingProfile;
-        //             }
-        //         }
-        //         // Add profile to array if it doesn't exist otherwise simply add the current
-        //         // sponsorship
-        //         if (!$exist) {
-        //             // Modify visible columns:
-        //             // -for profiles
-        //             $profile->makeHidden(['id', 'user_id', 'created_at', 'updated_at', 'pivot.profile_id', 'pivot']);
-        //             // -for users
-        //             $profile->user->makeHidden(['id', 'email', 'email_verified_at', 'home_address', 'created_at', 'updated_at']);
-        //             // -for specializations
-        //             if ($profile->user->relationLoaded('specializations') && $profile->user->specializations) {
-        //                 $profile->user->specializations->makeHidden(['id', 'created_at', 'updated_at', 'pivot']);
-        //             }
-
-        //             $profilesInSponsorships[] = $profile;
-        //         } else {
-        //             /** @var \App\Models\Profile $sameProfile */
-        //             $sameProfile->allSponsorships[] = $sponsorship_name;
-        //         }
-        //     }
-        // };
-
-        // // Sort by user's last_name (surname)
-        // $sortedProfiles = collect($profilesInSponsorships)->sortByDesc(function ($profile) {
-        //     return $profile->pivot->start_date;
-        // })->values();
 
         // Pagination
         $perPage = $request->input('per_page', 10);
