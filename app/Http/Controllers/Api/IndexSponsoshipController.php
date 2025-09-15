e78d1329ca9f52297ba064d3eb8de181e86a11df
tommasoTacconi <tommaso.tacconi@gmail.com>
2025-09-15 17:03:10 +0200

    feat: Add activeSponsorship relation in Profile model; use active sponsoship for retrieving sponsored profiles in IndexSponsorshipController

--- app/Http/Controllers/Api/IndexSponsoshipController.php
+++ app/Http/Controllers/Api/IndexSponsoshipController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\TimeHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Profile;
 use App\Models\Sponsorship;
@@ -24,60 +25,90 @@ class IndexSponsoshipController extends Controller
 
     public function sponsored(Request $request)
     {
-        $sponsorshipsWithProfiles = Sponsorship::with([
-            'profiles.user.specializations'
-        ])->get();
-
-        // Build profiles array, divided per sponsorship's type
-        $profilesInSponsorships = array();
-        foreach ($sponsorshipsWithProfiles as $sponsorshipWithProfiles) {
-            $sponsorship_name = $sponsorshipWithProfiles->name;
-            foreach ($sponsorshipWithProfiles->profiles as $profile) {
-                // Add sponsorship name as a new attribute
-                $profile->sponsorships[] = $sponsorship_name;
-                // Consider if the profile is already present by another sponsorship
-                $exist = false;
-                $sameProfile = null;
-                foreach ($profilesInSponsorships as $existingProfile) {
-                    if ($existingProfile->id === $profile->id) {
-                        $exist = true;
-                        $sameProfile = $existingProfile;
-                    }
-                }
-                // Add profile to array if it doesn't exist otherwise simply add the current
-                // sponsorship
-                if (!$exist) {
-                    // Modify visible columns:
-                    // -for profiles
-                    $profile->makeHidden(['id', 'user_id', 'created_at', 'updated_at', 'pivot.profile_id', 'pivot']);
-                    // -for users
-                    $profile->user->makeHidden(['id', 'email', 'email_verified_at', 'home_address', 'created_at', 'updated_at']);
-                    // -for specializations
-                    if ($profile->user->relationLoaded('specializations') && $profile->user->specializations) {
-                        $profile->user->specializations->makeHidden(['id', 'created_at', 'updated_at', 'pivot']);
-                    }
-
-                    $profilesInSponsorships[] = $profile;
-                } else {
-                    /** @var \App\Models\Profile $sameProfile */
-                    $sameProfile->sponsorships[] = $sponsorship_name;
-                }
-            }
-        };
-
-        // Sort by user's last_name (surname)
-        $sortedProfiles = collect($profilesInSponsorships)->sortBy(function ($profile) {
-            return $profile->user->last_name ?? '';
+        $sponsoredProfiles = Profile::has('activeSponsorship')
+            ->with(['user.specializations', 'activeSponsorship'])->get();
+        $sortedSponsoredProfiles = $sponsoredProfiles->sortByDesc(function (object $sponsored, int $key) {
+            return $sponsored->activeSponsorship[0]['pivot']['start_date'];
         })->values();
 
+        // Modify visible columns in profiles, users and specializations:
+        $sponsoredProfiles->makeHidden([
+            'id',
+            'user_id',
+            'created_at',
+            'updated_at',
+            'user.id',
+            'user.email',
+            'user.email_verified_at',
+            'user.home_address',
+            'user.created_at',
+            'user.updated_at',
+            'user.specializations.id',
+            'user.specializations.created_at',
+            'user.specializations.updated_at',
+            'user.specializations.pivot',
+            'active_sponsorship.id',
+            'active_sponsorship.created_at',
+            'active_sponsorship.updated_at',
+            'active_sponsorship.pivot.profile_id',
+            'active_sponsorship.pivot.sponsorship_id',
+        ]);
+        // $profile->user->makeHidden(['id', 'email', 'email_verified_at', 'home_address', 'created_at', 'updated_at']);
+        // -for specializations
+        // if ($profile->user->relationLoaded('specializations') && $profile->user->specializations) {
+        //     $profile->user->specializations->makeHidden(['id', 'created_at', 'updated_at', 'pivot']);
+        // }
+        // // Build profiles array, divided per sponsorship's type
+        // $profilesInSponsorships = array();
+        // foreach ($sponsorshipsWithProfiles as $sponsorshipWithProfiles) {
+        //     $sponsorship_name = $sponsorshipWithProfiles->name;
+        //     foreach ($sponsorshipWithProfiles->profiles as $profile) {
+        //         // Add sponsorship name as a new attribute
+        //         $profile->allSponsorships = [];
+        //         $profile->allSponsorships = array_merge($profile->allSponsorships, [$sponsorship_name]);
+        //         // Consider if the profile is already present by another sponsorship
+        //         $exist = false;
+        //         $sameProfile = null;
+        //         foreach ($profilesInSponsorships as $existingProfile) {
+        //             if ($existingProfile->id === $profile->id) {
+        //                 $exist = true;
+        //                 $sameProfile = $existingProfile;
+        //             }
+        //         }
+        //         // Add profile to array if it doesn't exist otherwise simply add the current
+        //         // sponsorship
+        //         if (!$exist) {
+        //             // Modify visible columns:
+        //             // -for profiles
+        //             $profile->makeHidden(['id', 'user_id', 'created_at', 'updated_at', 'pivot.profile_id', 'pivot']);
+        //             // -for users
+        //             $profile->user->makeHidden(['id', 'email', 'email_verified_at', 'home_address', 'created_at', 'updated_at']);
+        //             // -for specializations
+        //             if ($profile->user->relationLoaded('specializations') && $profile->user->specializations) {
+        //                 $profile->user->specializations->makeHidden(['id', 'created_at', 'updated_at', 'pivot']);
+        //             }
+
+        //             $profilesInSponsorships[] = $profile;
+        //         } else {
+        //             /** @var \App\Models\Profile $sameProfile */
+        //             $sameProfile->allSponsorships[] = $sponsorship_name;
+        //         }
+        //     }
+        // };
+
+        // // Sort by user's last_name (surname)
+        // $sortedProfiles = collect($profilesInSponsorships)->sortByDesc(function ($profile) {
+        //     return $profile->pivot->start_date;
+        // })->values();
+
         // Pagination
-        $perPage = $request->input('per_page', 20);
+        $perPage = $request->input('per_page', 10);
         $currentPage = $request->input('page', 1);
-        $currentItems = $sortedProfiles->slice(($currentPage - 1) * $perPage, $perPage)->values()->all();
+        $currentItems = $sortedSponsoredProfiles->slice(($currentPage - 1) * $perPage, $perPage)->values()->all();
 
         $paginator = new LengthAwarePaginator(
             $currentItems,
-            $sortedProfiles->count(),
+            $sortedSponsoredProfiles->count(),
             $perPage,
             $currentPage,
             ['path' => $request->url(), 'query' => $request->query()]
--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Helpers\TimeHelper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -36,5 +37,15 @@ class Profile extends Model
     public function sponsorships()
     {
         return $this->belongsToMany(Sponsorship::class)->withPivot(['start_date', 'end_date']);
+    }
+
+    public function activeSponsorship()
+    {
+        $computedTime = TimeHelper::computeAppTime(false);
+        $activeSponsorship = $this->sponsorships()
+            ->wherePivot('start_date', '<', $computedTime)
+            ->wherePivot('end_date', '>', $computedTime);
+
+        return $activeSponsorship;
     }
 }
