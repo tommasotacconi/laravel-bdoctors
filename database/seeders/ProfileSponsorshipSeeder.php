06a4e561f76e195ec906c169be00a2ff033947c1
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-03-20 18:10:52 +0100

    fix: Correct seeding in profile_sponsorship to correctly cycle through previously attached sponsorships and prevent overlapping

--- database/seeders/ProfileSponsorshipSeeder.php
+++ database/seeders/ProfileSponsorshipSeeder.php
@@ -6,7 +6,6 @@ use App\Models\Profile;
 use App\Models\Sponsorship;
 use Carbon\Carbon;
 use Carbon\CarbonImmutable;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 
@@ -19,36 +18,39 @@ class ProfileSponsorshipSeeder extends Seeder
     public function run(Faker $faker): void
     {
         $profiles = Profile::all();
-
         $sponsorships = Sponsorship::all()->pluck('duration', 'id')->all();
         // Specify a year end for the demo app profiles
         $selectedYearEnd = Carbon::create(2024, 12, 31, 23, 59, 59);
 
         foreach ($profiles as $profile) {
             $sponsorshipsNumber = rand(0, 30);
+            $addedSpons = [];
             for ($i = 0; $i < $sponsorshipsNumber; $i++) {
                 $sponsorshipId = $faker->randomKey($sponsorships);
                 $sponsorshipDuration = $sponsorships[$sponsorshipId];
 
                 do {
-                    $randomActivationDate = $faker->dateTimeBetween($profile->created_at, $selectedYearEnd, 'Europe/Rome');
-                    $randomActivationDate = CarbonImmutable::create($randomActivationDate);
+                    $start_date = $faker->dateTimeBetween($profile->created_at, $selectedYearEnd, 'Europe/Rome');
+                    $start_date = CarbonImmutable::create($start_date);
+                    $end_date = $start_date->addHours($sponsorshipDuration);
+                    $created_at = Carbon::now()->subDays(Carbon::now()->diffInDays($selectedYearEnd) + 367);
                     // check if there is another sponsorship active in the computed period
                     $isPossible = true;
-                    foreach ($profile->sponsorships as $sponsorship) {
-                        $start = $sponsorship->pivot->start_date;
-                        $end = $sponsorship->pivot->end_date;
-                        if ($randomActivationDate->between($start, $end))
+                    foreach ($addedSpons as ['start_date' => $start, 'end_date' => $end]) {
+                        if ($start_date->between($start, $end) || $end_date->between($start, $end) || $start_date < $start && $end_date > $end) {
                             $isPossible = false;
+                            break;
+                        }
                     }
                 } while ($isPossible === false);
 
-                $profile->sponsorships()->attach($sponsorshipId, [
-                    'start_date' => $randomActivationDate,
-                    'end_date' => $randomActivationDate->addHours($sponsorshipDuration),
-                    'created_at' => $randomActivationDate
-                ]);
+                foreach (['start_date', 'end_date', 'created_at'] as $el) {
+                    $sponDetails[$el] = $$el;
+                }
+                $profile->sponsorships()->attach($sponsorshipId, $sponDetails);
+                $addedSpons[] = ['id' => $sponsorshipId, ...$sponDetails];
             }
+            // print_r("\n\nprofile_id={$profile->id} sponsorships: " . json_encode($profile->sponsorships->map(fn($el) => $el->name)->all()) . "\n");
         }
     }
 }
