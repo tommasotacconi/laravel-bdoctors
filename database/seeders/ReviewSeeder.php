feba93923e80cd133d03a250b91cc82fc441916c
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-04-04 01:56:07 +0200

    feat: Scatter reviews average vote

--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 
-
 class ReviewSeeder extends Seeder
 {
     /**
@@ -17,19 +16,19 @@ class ReviewSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        $profileIds = Profile::all()->pluck("id");
+        Profile::all()->each(function ($profile) use ($faker) {
+            if ($profile->id > 100 && $profile->id < 240) return;
 
-        foreach ($profileIds as $profileId) {
-            if ($profileId !== 10 && $profileId !== 50 && $profileId !== 250 && $profileId > 100 && $profileId < 240) continue;
-
+            $meanVote =  rand(0, 5);
+            $maxDeviation = min(abs($meanVote - 0), abs($meanVote - 5));
             for ($i = 0; $i < ($faker->numberBetween(20, 40)); $i++) {
-                $newReview = new Review();
-                $newReview->profile_id = $profileId;
-                $newReview->vote = $faker->numberBetween(1, 5);
-                $newReview->content = $faker->realText(rand(50, 150));
-                $newReview->email = $faker->email();
-                $newReview->first_name = $faker->firstName();
-                $newReview->last_name = $faker->lastName();
+                $reviewData = [
+                    'vote' => $meanVote + $faker->randomElement([1, -1]) * $maxDeviation,
+                    'content' => $faker->realText(rand(50, 150)),
+                    'last_name' => $ln = $faker->lastName(),
+                    'first_name' => $fn = $faker->firstName(),
+                    'email' => str_replace(' ', '', strtolower("$fn.$ln@")) . $faker->safeEmailDomain(),
+                ];
                 // Generate random date, excluding non existing hour due to DST
                 $DST_from = Carbon::create(2024, 3, 31, 2, 0, 0);
                 $DST_to = Carbon::create(2024, 3, 31, 3, 0, 0);
@@ -37,11 +36,12 @@ class ReviewSeeder extends Seeder
                     $startDate = Carbon::create(2024, 1, 1, 0, 0, 0);
                     $endDate = Carbon::create(2024, 12, 31, 23, 59, 59);
                     $randomDate = $faker->dateTimeBetween($startDate, $endDate);
-                } while (Carbon::parse($randomDate)->isBetween($DST_from, $DST_to));
-                $newReview->created_at = $randomDate;
-                $newReview->updated_at = $randomDate;
-                $newReview->save();
+                    $isNotValid = Carbon::parse($randomDate)->isBetween($DST_from, $DST_to);
+                    \Log::info("sponsorship start date is " . ($isNotValid ? 'not' : '') . " valid", []);
+                } while ($isNotValid);
+                $reviewData['created_at'] = $randomDate;
+                $profile->reviews()->save((new Review())->fill($reviewData));
             }
-        }
+        });
     }
 }
