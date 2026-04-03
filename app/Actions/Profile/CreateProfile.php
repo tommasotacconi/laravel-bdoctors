8635bbf6480b89b929b9dfbf4bd261236096f5c5
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-04-03 18:14:02 +0200

    refactor: Cycle through $fileDirs only

--- app/Actions/Profile/CreateProfile.php
+++ app/Actions/Profile/CreateProfile.php
@@ -8,13 +8,15 @@ use App\Models\User;
 
 class CreateProfile
 {
-    public function __construct(protected SetFileField $sFF) {}
+    public function __construct(protected SetFileField $sFF)
+    {
+    }
 
     public function handle(User $user, array $data): Profile
     {
-        $fileDir = ['photo' => 'photos', 'curriculum' => 'curricula'];
-        foreach ($data as $field => $value) {
-            if (in_array($field, ['photo', 'curriculum'])) $data[$field] = $this->sFF->handle($data[$field] ?? null, $fileDir[$field]);
+        $fileDirs = ['photo' => 'photos', 'curriculum' => 'curricula'];
+        foreach ($fileDirs as $field => $dir) {
+            $data[$field] = $this->sFF->handle($data[$field] ?? null, $dir);
         }
         $profile = $user->profile()->create($data);
         $profile->user->makeVisible('home_address');
