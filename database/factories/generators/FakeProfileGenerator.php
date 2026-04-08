b500a9c52b27d48a61a625c1d9723abcc2754c5f
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-04-08 15:28:57 +0200

    feat: Improve UserSeeder asking for users number in console and using promise for profile photo requests

--- database/factories/generators/FakeProfileGenerator.php
+++ database/factories/generators/FakeProfileGenerator.php
@@ -5,6 +5,8 @@ namespace Database\Factories\Generators;
 use App\Helpers\TimeHelper;
 use App\Models\User;
 use Carbon\Carbon;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Database\Seeder;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Http;
@@ -14,8 +16,9 @@ class FakeProfileGenerator extends Seeder
     /**
      * Run the database seeds.
      */
-    public static function make(?string $gender = null, ?User $user = null): array
+    public static function make(?string $gender = null, bool $hasPhoto = false, ?User $user = null): array
     {
+        $client = new Client();
         $faker = fake();
         $gender ??= $faker->randomElement(['male', 'female']);
         $avatarsBaseUrl = 'https://api.dicebear.com/9.x/avataaars/svg';
@@ -33,28 +36,31 @@ class FakeProfileGenerator extends Seeder
             ]
         ];
 
-        $photo = null;
-        if (in_array($user?->id, array_merge([10, 50, 250, 200], range(11, 30), range(180, 189), range(235, 239)))) {
-            $content = Http::get($avatarsBaseUrl, [
+        if ($hasPhoto) {
+            $url = "$avatarsBaseUrl?" . http_build_query([
                 'seed' => $user->email,
                 ...$params['inCommon'],
                 ...$params[$gender]
-            ])->body();
-            $photo = UploadedFile::fake()->createWithContent('profile_photo.svg', $content);
+            ]);
+            $promise = $client->getAsync($url)->then(fn ($res) => UploadedFile::fake()->createWithContent(
+                'profile_photo.svg',
+                $res->getBody()->getContents()
+            ));
+            // \Log::info("started request for user $user->email", [$gender, 'status' => $promises[$user->id]->getState()]);
         }
-        if ($photo) \Log::info('url created for '. $user->first_name . '(id '. $user->id . ')', [$photo]);
         $curriculum = UploadedFile::fake()->createWithContent('cv.txt', $faker->realTextBetween(200, 1000));
 
         $yearEnd = TimeHelper::computeAppTime()->endOfYear();
 
-        return [
-            'user_id' => $user?->id ?? null,
-            'curriculum' => $curriculum,
-            'photo' => $photo,
-            'office_address' => $faker->streetAddress(),
-            'phone' => $faker->phoneNumber(),
-            'services' => $faker->realTextBetween(30, 100),
-            'created_at' => $user?->created_at->addDays($user->created_at->diffInDays($yearEnd) * rand(0, 5) / 100) ?? null
+        return [[
+                'user_id' => $user?->id ?? null,
+                'curriculum' => $curriculum,
+                'office_address' => $faker->streetAddress(),
+                'phone' => $faker->phoneNumber(),
+                'services' => $faker->realTextBetween(30, 100),
+                'created_at' => $user?->created_at->addDays($user->created_at->diffInDays($yearEnd) * rand(0, 5) / 100) ?? null,
+            ],
+            $promise ?? null
         ];
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,15 +3,15 @@
 namespace Database\Seeders;
 
 use App\Actions\Profile\CreateProfile;
+use App\Actions\SetFileField;
 use App\Actions\User\CreateUser;
 use App\Helpers\TimeHelper;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Profile;
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\Specialization;
-use Carbon\Carbon;
 use Database\Factories\Generators\FakeProfileGenerator;
 use Faker\Generator as Faker;
+use GuzzleHttp\Promise;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -19,51 +19,74 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
+    public function __construct(protected Faker $faker)
+    {
+    }
+
     /**
      * Run the database seeds.
      */
-    public function run(Faker $faker, FakeProfileGenerator $fakeProfGenerator, CreateUser $userCreator, CreateProfile $profileCreator): void
-    {
-        for ($i = 0; $i < 300; $i++) {
+    public function run(
+        FakeProfileGenerator $fakeProfGenerator,
+        CreateUser $userCreator,
+        CreateProfile $profileCreator
+    ): void {
+        $usersNum = (int) $this->command->ask('How many users?', 250);
+        if ($usersNum <= 0) {
+            $this->command->error('Count must be greater than 0');
+            return;
+        }
+        $profilesData = [];
+        $promises = [];
+
+        for ($i = 0; $i < $usersNum; $i++) {
             // Create two groups of hanonyms of test
             if ($i === 10 - 1 || $i === 50 - 1 || $i === 250 - 1) {
                 $gender = 'male';
-                $userData = $this->makeTestUser($faker, $i);
+                $userData = $this->makeTestUser($this->faker, $i);
             } elseif ($i === 200 - 1 || $i === 249 - 1) {
                 $gender = 'female';
-                $userData = $this->makeTestUser($faker, $i, 'Serena', 'Pesano');
+                $userData = $this->makeTestUser($this->faker, $i, 'Serena', 'Pesano');
             } else {
-                $gender = $faker->randomElement(['male', 'female']);
-                $fName = $userData['first_name'] = $faker->firstName($gender);
-                $lName = $userData['last_name'] = $faker->lastName($gender);
-                // ---Email generation
-                $eventualUsernameNum = $faker->boolean(20) ? $faker->randomNumber(2, true) : null;
-                $usernameComp = ["$fName{$faker->randomElement(['.', '-', '_', ''])}$lName", $eventualUsernameNum];
-                $userData['email'] = str_replace(' ', '', strtolower(implode($usernameComp)) . "@{$faker->freeEmailDomain()}"); // ---
-                $userData['password'] = Hash::make($faker->password(6, 20));
-                $userData['created_at'] = TimeHelper::computeAppTime()->startOfYear()->addDays(365 * rand(1, 90) / 100);
+                $gender = $this->faker->randomElement(['male', 'female']);
+                $userData = [
+                    'first_name' => $fName = $this->faker->firstName($gender),
+                    'last_name' => $lName = $this->faker->lastName($gender),
+                    'email' => $this->getRandEmail($fName, $lName),
+                    'password' => Hash::make($this->faker->password(6, 20)),
+                    'created_at' => TimeHelper::computeAppTime()->startOfYear()->addDays(365 * rand(1, 90) / 100)
+                ];
             }
-            $userData['home_address'] = $faker->streetAddress();
+            $userData['home_address'] = $this->faker->streetAddress();
             $user = $userCreator->handle($userData, false);
-            $profileCreator->handle($user, $fakeProfGenerator->make($gender, $user));
+            $withPhotoUsers = array_merge([10, 50, 250, 200], range(11, 60), range(150, 249));
+            if (in_array($user?->id, $withPhotoUsers)) $hasPhoto = true;
+            else $hasPhoto = false;
+            [$profileData, $promises[$user->id]] = $fakeProfGenerator->make($gender, $hasPhoto, $user);
+            $profilesData[$user->id] = [$user, $profileData];
         }
 
-        $homonymsName = $this->retrieveHoms();
-        if (!empty($homonymsName)) {
-            $homonyms = User::query()
-                ->whereIn(
-                    DB::raw('CONCAT(`first_name`, \' \', `last_name`)'),
-                    $homonymsName
-                )->orderBy('first_name')
-                ->orderBy('last_name')
-                ->orderBy('id')
-                ->get();
-            $homonymsGroups = array_fill_keys($homonymsName, 1);
-            $homonyms->each(function ($hom) use (&$homonymsGroups) {
-                $group = "$hom->first_name $hom->last_name";
-                $hom->update(['homonymous_id' => $homonymsGroups[$group]++]);
-            });
+        $responses = Promise\Utils::settle($promises)->wait();
+
+        foreach ($profilesData as $userId => [$user, $profileData]) {
+            $photoPath = $responses[$userId]['value'] ?? null;
+            if ($photoPath) $profileData['photo'] = $photoPath;
+            $createdProf[] = $profileCreator->handle($user, $profileData);
         }
+
+        $this->setHomonymsId();
+        \Log::info(
+            'created profiles ' . count($createdProf) .
+            ', of which ' . array_reduce($responses, fn ($acc, $item) => $item['value'] ?? null ? ++$acc : $acc, 0) . ' with photo'
+        );
+    }
+
+    protected function getRandEmail($fName, $lName)
+    {
+        $eventualUsernameNum = $this->faker->boolean(20) ? $this->faker->randomNumber(2, true) : null;
+        $usernameComp = ["$fName{$this->faker->randomElement(['.', '-', '_', ''])}$lName", $eventualUsernameNum];
+
+        return  str_replace(' ', '', strtolower(implode($usernameComp)) . "@{$this->faker->freeEmailDomain()}");
     }
 
     protected function makeTestUser($fakerInstance, $ind, $fName = 'Adriano', $lName = 'Carola')
@@ -85,13 +108,30 @@ class UserSeeder extends Seeder
         return $userData;
     }
 
-    protected function retrieveHoms()
+    protected function setHomonymsId()
     {
-        return DB::table('users')
+        $homonymsName = DB::table('users')
             ->selectRaw('CONCAT(first_name, " ", last_name) as full_name, COUNT(*) as cnt')
             ->groupBy('full_name')
             ->having('cnt', '>', 1)
             ->pluck('full_name')
             ->all();
+
+        if (!empty($homonymsName)) {
+            $homonyms = User::query()
+                ->whereIn(
+                    DB::raw('CONCAT(`first_name`, \' \', `last_name`)'),
+                    $homonymsName
+                )->orderBy('first_name')
+                ->orderBy('last_name')
+                ->orderBy('id')
+                ->get();
+            $homonymsGroups = array_fill_keys($homonymsName, 1);
+            $homonyms->each(function ($hom) use (&$homonymsGroups) {
+                $group = "$hom->first_name $hom->last_name";
+                $hom->update(['homonymous_id' => $homonymsGroups[$group]++]);
+            });
+        }
+
     }
 }
